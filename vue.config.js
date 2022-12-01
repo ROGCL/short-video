33523f53ae088ac9a83b2d7ebb1667dbaeda369a
ROGCL <anj5249@gmail.com>
2022-12-01 20:36:25 +0800

    12/01github

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     lintOnSave: false,
-    publicPath: '/media/page/painting/' 
+    // publicPath: '/media/page/painting/' 
          //记得打包时展开
     // publicPath:'/media/page/painting_test/'
 })
